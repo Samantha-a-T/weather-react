d5d875ee425e3a4f25dfc23f0057edc66847b286
Samantha-a-T <115659673+Samantha-a-T@users.noreply.github.com>
2022-12-14 18:51:15 -0700

    Add styling to weather app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Weather2 from "./Weather2"
+import Weather from "./Weather"
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather Search</h1>
-        <Weather2 defaultCity="Calgary" />
+   
+        <Weather defaultCity="Calgary" />
         <footer>
           <a href="https://github.com/Samantha-a-T/weather-react">
             Open-source code by{" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,59 +1,65 @@
 import React, { useState } from "react";
 import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
-export default function Weather() {
-  let [description, setDescription] = useState();
-  let [temperature, setTemperature] = useState();
-  let [humidity, setHumidity] = useState();
-  let [wind, setWind] = useState();
-  let [icon, setIcon] = useState();
-  let [city, setCity] = useState();
-  let [loaded, setLoaded] = useState(false);
+export default function Weather(props) {
 
-  function weatherSearch(response) {
-    setLoaded(true);
-    setDescription(response.data.weather[0].description);
-    setTemperature(response.data.main.temp);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-    setIcon(response.data.weather[0].icon);
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity]= useState(props.defaultCity)
+  function handleResponse(response) {
+    setWeatherData({
+      ready:true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      date: new Date(response.data.dt * 1000),
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+    });
   }
+function search(){
+ const apiKey = "cecd230221b0d2dca5201b2f2ec509a6";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(handleResponse);
+}
 
-  function axiosSearch(event) {
+  function handleSubmit(event){
     event.preventDefault();
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cecd230221b0d2dca5201b2f2ec509a6&units=metric`;
-    axios.get(apiUrl).then(weatherSearch);
+    search();
   }
-
-  function changeCity(event) {
-    event.preventDefault();
-    setCity(event.target.value);
+  function handleCityChange(event){
+setCity(event.target.value)
   }
-  let form = (
-    <form className="Search" onSubmit={axiosSearch}>
-      <input type="search" placeholder="Type a city..." onChange={changeCity} />
-      <input type="submit" />
-    </form>
-  );
-
-  if (loaded) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
-        {form}
-        <ul>
-          <li> {description} </li>
-          <li> The temperature is currently {Math.round(temperature)}°C </li>
-          <li> The humidity is currently {Math.round(humidity)}% </li>
-          <li> The wind is currently {Math.round(wind)}km/h </li>
-          <img
-            src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
-            alt={description}
-          />
-        </ul>
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn w-100 button"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data ={weatherData}/>
       </div>
     );
   } else {
-    return <div>{form}</div>;
+   search();
+    return "Loading data...";
   }
 }
